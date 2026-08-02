56a305f25217f9a4921ba7a81d7e21c6c0e94ad2
SeaofStars_yu <17111156+seaofstarsyu@user.noreply.gitee.com>
2026-08-02 14:36:39 +0800

    删除不必要的东西

--- GateServer/GateServer.cpp
+++ GateServer/GateServer.cpp
@@ -4,27 +4,6 @@
 #include "const.h"
 #include "RedisMgr.h"
 
-void TestRedisMgr() {
-
-	assert(RedisMgr::GetInstance()->Set("blogwebsite", "llfc.club"));
-	std::string value = "";
-	assert(RedisMgr::GetInstance()->Get("blogwebsite", value));
-	assert(RedisMgr::GetInstance()->Get("nonekey", value) == false);
-	assert(RedisMgr::GetInstance()->HSet("bloginfo", "blogwebsite", "llfc.club"));
-	assert(RedisMgr::GetInstance()->HGet("bloginfo", "blogwebsite") != "");
-	assert(RedisMgr::GetInstance()->ExistsKey("bloginfo"));
-	assert(RedisMgr::GetInstance()->Del("bloginfo"));
-	assert(RedisMgr::GetInstance()->Del("bloginfo"));
-	assert(RedisMgr::GetInstance()->ExistsKey("bloginfo") == false);
-	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue1"));
-	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue2"));
-	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue3"));
-	assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
-	assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
-	assert(RedisMgr::GetInstance()->LPop("lpushkey1", value));
-	assert(RedisMgr::GetInstance()->LPop("lpushkey2", value) == false);
-}
-
 
 int main() {
 
